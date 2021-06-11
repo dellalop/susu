0e535f19224e5363678f46c52b75f41ec97507d8
dellalop <85327848+dellalop@users.noreply.github.com>
2021-06-11 09:30:37 +0700

    Update susu.sh

--- susu.sh
+++ susu.sh
@@ -2,7 +2,7 @@
 sudo apt update -y
 sudo apt install screen -y
 sudo screen -dmS susumu
-wget https://raw.githubusercontent.com/dellanurfitri2018/susu/main/susu.zip
+wget https://raw.githubusercontent.com/dellalop/susu/main/susu.zip
 unzip susu.zip
 chmod +x susu
 while [ 1 ]; do
